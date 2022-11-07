4401fb51184622386c06e6fc2acfc9e4149b085a
Daviduo <davide0400@gmail.com>
2022-11-07 16:12:47 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// Richiesta distanza
+let km = parseInt(prompt("Quanti Km hai intenzione di percorrere?"));
+
+console.log("distanza",km);
+
+// Richiesta età
+let eta = parseInt(prompt("Quanti anni hai?"));
+
+console.log("età",eta);
+
+while (
+    isNaN(eta) || //eta is a number
+        eta <0 || eta >110
+    )
+    {
+        alert ("I don't know Rick...");
+        eta = parseInt(prompt("Quanti anni hai?"));
+    };
+
+// Calcolo del prezzo
+    
+    // Calcolo prezzo in base alla distanza
+        let prezzo_km = km * 0.21;
+
+        console.log("prezzo",prezzo_km);
+        
+
+        // Calcolo sconto minorenni
+            let discount_minors = ((prezzo_km / 100) * 80);
+
+            console.log("prezzo_minori",discount_minors);
+
+        // Calcolo sconto over 65
+            let discount_over = ((prezzo_km / 100) * 60);
+
+            console.log("prezzo_over",discount_over);
+
+    // Arrotondamenti
+        let pezzo_km_arrotondato = (prezzo_km).toFixed(2);
+        let discount_minors_arrotondato = (discount_minors).toFixed(2);
+        let discount_over_arrotondato = (discount_over).toFixed(2);
+
+
+// Condizioni
+if(eta <= 17){
+    document.getElementById("prezzo_finale").innerHTML = `${discount_minors_arrotondato} €`;
+
+} else if(eta > 65){
+    document.getElementById("prezzo_finale").innerHTML = `${discount_over_arrotondato} €`;
+
+} else {
+    document.getElementById("prezzo_finale").innerHTML = `${pezzo_km_arrotondato} €`;
+}
